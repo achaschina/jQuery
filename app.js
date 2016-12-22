8a40be2d99801a5fedcde7ba43b4906be9ff2909
achaschina <anna200985@gmail.com>
2016-12-22 15:03:59 +0200

    added new files to my jQuery hw

---
+++ app.js
@@ -0,0 +1,50 @@
+function getTimeLeft() {
+    var timeLeft = getDifferentDate();
+    var seconds = Math.floor( (timeLeft/1000) % 60 );
+    var minutes = Math.floor( (timeLeft/1000/60) % 60 );
+    var hours = Math.floor( (timeLeft/(1000*60*60)) % 24 );
+    var days = Math.floor( timeLeft/(1000*60*60*24) );
+    return timeObj =  {
+        'Seconds'  : seconds,
+        'Minutes'  : minutes,
+        'Hours'    : hours,
+        'Days'     : days
+    }
+}
+
+function getDifferentDate() {
+    var deadLine = moment('2016-12-31 23:59:59', 'YYYY-MM-DD-hh-mm-ss');
+    var nowDate  = new Date();
+    var timeLeft = deadLine > nowDate ? Math.ceil((deadLine - nowDate)) : null;
+    return timeLeft;
+}
+
+function format(dig) {
+    dig = dig.toString();
+    var len = 2;
+    var result = '' + dig;
+    if (dig.length < len){
+        result = '0' + result;
+    }
+    return result;
+}
+
+function setTimeLeft() {
+
+}
+
+init();
+
+function init() {
+    var interval = setInterval(function () {
+        var timeObj = getTimeLeft();
+        for (var key in timeObj) {
+            var span = $('.count'+ key + ' .position .digit');
+            var value = format(timeObj[key]);
+            span[0].innerText = value;
+        }
+        if (getDifferentDate <= 0) {
+            clearInterval(interval);
+        }
+    }, 1000)
+}
